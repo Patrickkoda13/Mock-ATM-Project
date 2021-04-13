3fac4eb08d466fbbd720b90cae67dae4124e7280
Patrick E <patrickkoda@gmail.com>
2021-04-13 00:41:21 -0700

    Adding additional changes with new selections for users

--- Mock_ATM_Project.py
+++ Mock_ATM_Project.py
@@ -2,7 +2,7 @@ import datetime
 time = datetime.datetime.now()
 name = input("What is your name?\n")
 allowed_users= ["Patrick", "Love", "Kumi"]
-allowed_password = ['passwordPatrick', 'passwordKumi', 'passwordLove']
+allowed_password = ['passwordPatrick', 'passwordLove', 'passwordKumi']
 
 if(name in allowed_users):
     password = input("What is your password\n")
@@ -11,7 +11,7 @@ if(name in allowed_users):
     if(password == allowed_password[userID]):
         print("Welcome %s" % name)
         print(time)
-
+    while True:
         print("These are the available options:")
         print("1. Withdrawal")
         print("2. Cash Deposit")
@@ -22,13 +22,22 @@ if(name in allowed_users):
         if (selected_option == 1):
             print("You selected %s" % selected_option)
 
+            withdrawal = int(input("How much would you like to withdraw?"))
+            print("Take your cash.")
+
         elif(selected_option == 2):
             print("You selected %s" % selected_option)
+            deposit = int(input("How much would you like to deposit?"))
+            print(f"Your balance is now {deposit}")
         elif(selected_option == 3):
-            print("You selected %s 'Cash Deposit'" % selected_option)
+            print("You selected %s" % selected_option)
+            complaint = input("What issue would you like to report?")
+            print("Thank you for contacting us.")
         else:
             print("Invalid option selected, please try again")
+
     else:
         print("Password Incorrect, please try again")
+
 else:
     print("Name not found, please try again")
